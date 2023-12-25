d6369884715fab8a995f554d6c11d81fbc31fb1a
MlepNos <kaan.case2110@gmail.com>
2023-12-25 13:45:53 +0100

    configured connection

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,11 @@ const sql = require("mssql");
 // env
 require("dotenv").config();
 
+// express app
 const app = express();
+
+// routes
+const calendarRoutes = require("./routes/calendar");
 
 // configure db connection
 const config = {
@@ -23,12 +27,33 @@ const config = {
 
 // Create a pool of database connections
 const pool = new sql.ConnectionPool(config);
-const poolConnect = pool.connect();
+//const poolConnect = pool.connect();
 
-// Express middleware to parse incoming JSON requests
+// Express middleware to parse incoming JSON requests (for using req.body)
 app.use(express.json());
 
-// Define a simple API endpoint
+// global middleware
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+//app.use("/api/calendar", calendarRoutes);
+
+pool
+  .connect()
+  .then(() => {
+    // listen for requests only if we are connected
+    app.listen(process.env.PORT, () => {
+      console.log(
+        "connected to db and Listening on port : " + process.env.PORT
+      );
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+/* Define a simple API endpoint
 app.get("/api/calendar", async (req, res) => {
   try {
     await poolConnect;
@@ -38,9 +63,9 @@ app.get("/api/calendar", async (req, res) => {
     console.error("SQL error", err);
     res.status(500).send("Internal Server Error");
   }
-});
-
+}); 
 // Start the server
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on http://localhost:${process.env.PORT}`);
 });
+ */
